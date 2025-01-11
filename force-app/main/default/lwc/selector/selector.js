aca99c9f4abde64a664e297bf62024644e2c6585
Delta0077 <adas0262@gmail.com>
2025-01-12 01:47:55 +0530

    Updating Selector js file
    Line 1 imports the <b>wire</b> service from <b>lwc</b>.
    
    Line 2 imports the adapterId and adapter-modules for getRecord and getFieldValue from lightning/uiRecordApi.
    
    Line 3 imports the current User Id using the @salesforce module.
    
    Line 4 imports the @salesforce schema for User.Name.
    
    Line 14 uses the @wire decorator to use the wire service to call getRecord passing in the userId and getting the fields.
    
    Line 15 sets user as the receiver for the @wire call.

--- force-app/main/default/lwc/selector/selector.js
+++ force-app/main/default/lwc/selector/selector.js
@@ -1,4 +1,8 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import {getRecord, getFieldValue} from 'lightning/uiRecordApi';
+import Id from '@salesforce/user/Id';
+import NAME_FIELD from '@salesforce/schema/User.Name';
+const FIELDS = [NAME_FIELD];
 
 export default class Selector extends LightningElement {
     selectedProductId;
@@ -6,4 +10,10 @@ export default class Selector extends LightningElement {
     handleProductSelected(evt) {
         this.selectedProductId = evt.detail;
     }
+    userId = Id;
+    @wire(getRecord, {recordId: '$userId', FIELDS})
+    user;
+    get name() {
+    return getFieldValue(this.user.data, NAME_FIELD);
+}
 }
